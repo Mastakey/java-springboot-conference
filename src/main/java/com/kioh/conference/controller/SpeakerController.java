3a2e0ecf6cd6802a06e27c89c24f4630084cc874
Kioh Han <kioh.han@gmail.com>
2019-11-24 15:20:12 -0500

    add Excepction handling

--- src/main/java/com/kioh/conference/controller/SpeakerController.java
+++ src/main/java/com/kioh/conference/controller/SpeakerController.java
@@ -6,6 +6,7 @@ import com.kioh.conference.dto.SpeakerDTO;
 import com.kioh.conference.service.SpeakerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class SpeakerController {
@@ -28,21 +30,40 @@ public class SpeakerController {
 
     @GetMapping(value = "/speakers/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public SpeakerDTO get(@PathVariable Long id) {
-        return speakerService.findOne(id);
+        try {
+            return speakerService.findOne(id);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+        
     }
 
     @PostMapping(value = "/speakers", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public SpeakerDTO add(@RequestBody SpeakerDTO newSpeaker){
-        return speakerService.create(newSpeaker);
+        try {
+            return speakerService.create(newSpeaker);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+        
     }
 
     @PutMapping(value = "/speakers/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public SpeakerDTO update(@RequestBody SpeakerDTO newSpeaker, @PathVariable Long id) {
-        return speakerService.update(newSpeaker, id);
+        try {
+            return speakerService.update(newSpeaker, id);
+        }catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 
     @DeleteMapping("/speakers/{id}")
     public String delete(@PathVariable Long id) {
-        return speakerService.delete(id);
+        try {
+            return speakerService.delete(id);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+        
     }
 }
